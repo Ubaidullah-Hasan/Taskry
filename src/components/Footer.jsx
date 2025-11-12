99e14c1b93618ea705798518931f7405aa1a8073
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-11-12 19:17:11 +0600

    setup, header, footer, & hero section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+
+
+    return (
+        <footer className="py-6 md:py-8 bg-amber-300">
+            <div className="container mx-auto">
+                <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+                    Copyright ©2024 | All rights reserved by Learn with Sumit
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
