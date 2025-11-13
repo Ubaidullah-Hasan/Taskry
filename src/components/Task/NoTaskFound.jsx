fbc659827099ab302d918b43d78d5e01d9ccf1fe
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-11-13 12:44:48 +0600

    no task found

---
+++ src/components/Task/NoTaskFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoTaskFound = () => {
+    return (
+        <div className='text-3xl text-center'>
+            No Task Found!
+        </div>
+    );
+};
+
+export default NoTaskFound;
--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -3,6 +3,7 @@ import TaskAction from "./TaskAction";
 import TaskList from "./TaskLIst";
 import TaskSearch from "./TaskSearch";
 import AddTaskModal from "./AddTaskModal";
+import NoTaskFound from "./NoTaskFound";
 
 
 export default function TaskBoard() {
@@ -98,12 +99,16 @@ export default function TaskBoard() {
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TaskAction onAddClick={() => setShowAddTaskModal(true)} onDeleteAll={handleDeleteAllTask} />
 
-                    <TaskList
-                        tasks={tasks}
-                        onEdit={handleEditTask}
-                        onDelete={handleDeleteTask}
-                        onFav={handleTaskFavourite}
-                    />
+                    {
+                        tasks.length > 0 ? (
+                            <TaskList
+                                tasks={tasks}
+                                onEdit={handleEditTask}
+                                onDelete={handleDeleteTask}
+                                onFav={handleTaskFavourite}
+                            />
+                        ) : <NoTaskFound />
+                    }
                 </div>
             </div>
         </section>
